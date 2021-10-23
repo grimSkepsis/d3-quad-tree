6c7f0288c844d10a64c6ed64a7ca5736a373a5ae
grimSkepsis <developer.mikehofer@gmail.com>
2021-10-23 07:34:28 -0700

    Adds d3 hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import * as d3 from "d3";
 
 const App = () => {
+  useEffect(() => {
+    d3.select("#view").append("span").text("Hello, world!");
+  });
 
   return (
     <div>
-      test
+      <div id="view"></div>
     </div>
   );
 };
