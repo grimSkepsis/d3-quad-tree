c56c93e1076c8b91318805d9384db3e1c8f9ce4a
grimSkepsis <developer.mikehofer@gmail.com>
2021-10-23 07:14:21 -0700

    Adds initial react project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+
+  return (
+    <div>
+      test
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
